837e6a02d7ae625b8b5c701a435df0b2ee6ec3e2
wangding0421 <wangding0421@gmail.com>
2014-02-25 14:07:37 +0000

    What have been done till now: 	1. Show a map; 	2. Change the map default properties including: 		(1). Starting position at HKUST; 		(2). Zoom in so that HKUST is bigger at the first glance; 		(3). Padding the map with 100 pixels upper so that the below 100 pixels could be used for UI team; 	3. Skipped the step "Verify Map availability";
    What am I going to do:
    	1. Add a Marker;
    	2. Add titles and contents for the marker;

--- Localender/src/com/comp3111h/localender/MainActivity.java
+++ Localender/src/com/comp3111h/localender/MainActivity.java
@@ -1,22 +1,32 @@
 package com.comp3111h.localender;
 
-import android.os.Bundle;
+import android.annotation.SuppressLint;
 import android.app.Activity;
-import android.view.Menu;
+import android.os.Bundle;
+
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.MapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
 
 public class MainActivity extends Activity {
 
+	
+	private GoogleMap localenderMap;
+	
+    @SuppressLint("NewApi")
 	@Override
-	protected void onCreate(Bundle savedInstanceState) {
-		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_main);
-	}
-
-	@Override
-	public boolean onCreateOptionsMenu(Menu menu) {
-		// Inflate the menu; this adds items to the action bar if it is present.
-		getMenuInflater().inflate(R.menu.main, menu);
-		return true;
-	}
-
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        localenderMap = ((MapFragment) getFragmentManager().findFragmentById(R.id.map)).getMap();
+        localenderMap.setPadding(0, 0, 0, 100);
+        
+        //Add a marker for testing, do not add it in the onCreate method!
+        localenderMap.addMarker(new MarkerOptions()
+        .position(new LatLng(0, 0))
+        .title("Hello world"));            
+    }
+    
+    
 }
