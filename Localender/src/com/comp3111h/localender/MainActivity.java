85d9761b960b0d57556e7a49a5165dd2c874246a
wangding0421 <wangding0421@gmail.com>
2014-02-25 16:59:43 +0000

    What have been done till now: 	1. Show a map; 	2. Change the map default properties including: 		(1). Starting position at HKUST; 		(2). Zoom in so that HKUST is bigger at the first glance; 		(3). Padding the map with 100 pixels upper so that the below 100 pixels could be used for UI team; 	3. Skipped the step "Verify Map availability"; 	4. Add a Marker; 	5. Test the Drag and Click listener of Marker;
    What am I going to do:
    	1. Edit the info Window properties;
    	2. Search for a position;

--- Localender/src/com/comp3111h/localender/MainActivity.java
+++ Localender/src/com/comp3111h/localender/MainActivity.java
@@ -5,14 +5,19 @@ import android.app.Activity;
 import android.os.Bundle;
 
 import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.GoogleMap.OnMarkerDragListener;
 import com.google.android.gms.maps.MapFragment;
 import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
 
 public class MainActivity extends Activity {
 
 	
 	private GoogleMap localenderMap;
+	
+    //This is just adding a marker for testing, do not add it until an Event is created!
+	private Marker testMarker;
 	
     @SuppressLint("NewApi")
 	@Override
@@ -21,12 +26,57 @@ public class MainActivity extends Activity {
         setContentView(R.layout.activity_main);
         localenderMap = ((MapFragment) getFragmentManager().findFragmentById(R.id.map)).getMap();
         localenderMap.setPadding(0, 0, 0, 100);
+                
+        //This pieces of codes are testing the OnMarkerClickListener's availability
+        /*
+        localenderMap.setOnMarkerClickListener(new OnMarkerClickListener() {
+			
+			@Override
+			public boolean onMarkerClick(Marker arg0) {
+				// 
+				if(arg0.equals(testMarker)){
+					arg0.setVisible(false);
+					return true;
+				}
+				return false;
+			}
+		});
+        */
         
-        //Add a marker for testing, do not add it in the onCreate method!
-        localenderMap.addMarker(new MarkerOptions()
-        .position(new LatLng(0, 0))
-        .title("Hello world"));            
-    }
-    
-    
+        //This pieces of codes are testing the OnMarkerDragListener's availability
+        /*
+        localenderMap.setOnMarkerDragListener(new OnMarkerDragListener() {
+			
+			@Override
+			public void onMarkerDragStart(Marker arg0) {
+				// 
+				if(arg0.equals(testMarker)){
+					arg0.setVisible(true);
+				}
+			}
+			
+			@Override
+			public void onMarkerDragEnd(Marker arg0) {
+				// 
+				if(arg0.equals(testMarker)){
+					arg0.setVisible(true);
+				}
+			}
+			
+			@Override
+			public void onMarkerDrag(Marker arg0) {
+				// 
+				if(arg0.equals(testMarker)){
+					arg0.setVisible(false);
+				}
+			}
+		});
+        */
+        
+        //Do not add it in the onCreate method!
+        testMarker = localenderMap.addMarker(new MarkerOptions()
+        .position(new LatLng(22.3375, 114.2630))
+        .title("COMP3111H Lecture").snippet("Today\n15:00 - 16:30\nLT-E").draggable(true));
+        
+    }    
 }
