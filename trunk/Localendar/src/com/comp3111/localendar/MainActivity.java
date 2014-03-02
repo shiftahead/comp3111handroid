94c466defd5e448eda3f6e63d131072a31d4216b
zhangtian1000@gmail.com <zhangtian1000@gmail.com@6e385ed8-77c6-a1b5-9d71-b316007c340f>
2014-03-02 13:57:27 +0000

    main activity

--- trunk/Localendar/src/com/comp3111/localendar/MainActivity.java
+++ trunk/Localendar/src/com/comp3111/localendar/MainActivity.java
@@ -3,33 +3,48 @@ package com.comp3111.localendar;
 
 import java.util.ArrayList;
 
-import android.os.Bundle;
 import android.app.Activity;
+import android.os.Bundle;
 import android.support.v4.view.PagerAdapter;
 import android.support.v4.view.ViewPager;
 import android.support.v4.view.ViewPager.OnPageChangeListener;
 import android.util.DisplayMetrics;
 import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.WindowManager;
 import android.view.animation.Animation;
 import android.view.animation.TranslateAnimation;
 import android.widget.ImageView;
 
-public class MainActivity extends Activity {
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.GoogleMap.OnMarkerClickListener;
+import com.google.android.gms.maps.GoogleMap.OnMarkerDragListener;
+import com.google.android.gms.maps.GoogleMap.OnMyLocationButtonClickListener;
+import com.google.android.gms.maps.model.Marker;
+import com.google.android.gms.maps.MapFragment;
+import com.google.android.gms.maps.UiSettings;
 
+
+public class MainActivity extends Activity {
 	public static MainActivity instance = null;
-	
 	private ViewPager pager;	
 	private ImageView image, tab0, tab1, tab2;
 	private int currentTabIndex = 0;
 	private LayoutInflater inflator;
 	private int animationShiftOneScale, animationShiftTwoScale;
 	
+	// lcalendar is map object
+	MyGoogleMap localendar = new MyGoogleMap();
+	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
+		// map activities  
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+		
 		getWindow().setSoftInputMode(WindowManager.LayoutParams.SOFT_INPUT_STATE_ALWAYS_HIDDEN); 
         instance = this;
         
@@ -81,16 +96,39 @@ public class MainActivity extends Activity {
 		};
 		pager.setAdapter(myPagerAdapter);
 		pager.setCurrentItem(1);
-
+		localendar.localenderMap = ((MapFragment) getFragmentManager().findFragmentById(R.id.map)).getMap();
+		localendar.setMap();    
 	}
-	/*
+	/* set the pop up menu
+	 * in this menu people can set up map type
+	 */
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
 		// Inflate the menu; this adds items to the action bar if it is present.
-		getMenuInflater().inflate(R.menu.main, menu);
+		MenuInflater inflater = getMenuInflater(); 
+		inflater.inflate(R.menu.main, menu); 
 		return true;
 	}
-	*/
+	public boolean onOptionsItemSelected(MenuItem item){
+		switch (item.getItemId()) {
+		case R.id.mapNormal :
+			localendar.localenderMap.setMapType(GoogleMap.MAP_TYPE_NORMAL);
+			return true;
+		case R.id.mapSatellite :
+			localendar.localenderMap.setMapType(GoogleMap.MAP_TYPE_SATELLITE);
+			return true;
+		case R.id.mapHybrid :
+			localendar.localenderMap.setMapType(GoogleMap.MAP_TYPE_HYBRID);
+			return true;
+		}
+		return false;
+	}
+	
+	
+	/* Class to be defined
+	 * 1. MyOnClickListener
+	 * 2. MyOnPageChangeListener
+	 */
 	public class MyOnClickListener implements View.OnClickListener {
 		private int index = 0;
 
