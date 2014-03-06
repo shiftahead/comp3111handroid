7b9c0bf8c8bf5d248451bd6637a720c4a09603d7
jeremy.yangji <jeremy.yangji@gmail.com>
2014-03-06 18:59:19 +0000

    UI changes.

--- Localendar/src/com/comp3111/localendar/AddEventActivity.java
+++ Localendar/src/com/comp3111/localendar/AddEventActivity.java
@@ -4,6 +4,7 @@ import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.view.WindowManager;
 import android.view.View.OnClickListener;
 import android.widget.ArrayAdapter;
 import android.widget.Spinner;
@@ -13,6 +14,7 @@ public class AddEventActivity extends Activity implements OnClickListener{
 	protected void onCreate(Bundle savedInstanceState) {	
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.add_event_dialog);
+		getWindow().setSoftInputMode(WindowManager.LayoutParams.SOFT_INPUT_STATE_ALWAYS_HIDDEN); 
 		Spinner dropdown = (Spinner)findViewById(R.id.transportation_chooser);
 		String[] items = new String[]{"Drive", "Public transportation", "On foot"};
 		ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_item, items);
