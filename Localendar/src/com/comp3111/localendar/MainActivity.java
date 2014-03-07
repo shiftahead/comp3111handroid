fa175fde9fcd3389f4b96400ba3ec1e2068e0931
jeremy.yangji <jeremy.yangji@gmail.com>
2014-03-07 12:45:05 +0000

    Bottom bar layout changed.

--- Localendar/src/com/comp3111/localendar/MainActivity.java
+++ Localendar/src/com/comp3111/localendar/MainActivity.java
@@ -5,6 +5,7 @@ import java.util.ArrayList;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.support.v4.view.PagerAdapter;
 import android.support.v4.view.ViewPager;
@@ -14,6 +15,7 @@ import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
+import android.view.MotionEvent;
 import android.view.View;
 import android.view.WindowManager;
 import android.view.animation.Animation;
@@ -28,8 +30,8 @@ import com.google.android.gms.maps.MapFragment;
 public class MainActivity extends Activity implements View.OnClickListener{
 	public static MainActivity instance = null;
 	private ViewPager pager;	//view pager
-	private ImageView image, tab0, tab1, tab2;	//tabs for list, map and settings
-	private TextView text0, text1, text2;	//text tabs for list, map and settings
+	private ImageView image, tab0, tab1, tab2, addView;	//tabs for list, map and settings
+	private TextView text0, text1, text2, addText;	//text tabs for list, map and settings
 	private int currentTabIndex;	//tab index, maps is set default 
 	private int animationShiftOneScale, animationShiftTwoScale;	//animation effect
 	private int screenWidth;
@@ -67,7 +69,12 @@ public class MainActivity extends Activity implements View.OnClickListener{
         text0.setOnClickListener(new MyOnClickListener(0));
         text1.setOnClickListener(new MyOnClickListener(1));
         text2.setOnClickListener(new MyOnClickListener(2));
-
+        
+        addView = (ImageView) findViewById(R.id.add_event_button);
+        addText = (TextView) findViewById(R.id.add_event_text);
+        addView.setOnTouchListener(new MyOnTouchListener());
+        addText.setOnTouchListener(new MyOnTouchListener());
+        
         //get screen pixel to fulfill animation
         DisplayMetrics metrics = new DisplayMetrics();
         getWindowManager().getDefaultDisplay().getMetrics(metrics);
@@ -143,6 +150,33 @@ public class MainActivity extends Activity implements View.OnClickListener{
 		}
 		return false;
 	}
+	
+	@Override
+	public void onClick(View v) {
+		switch(v.getId()) {
+		case R.id.about_us_arrow:
+		case R.id.about_us: {
+			Intent intent = new Intent (this, AboutusActivity.class);			
+			startActivity(intent);
+			overridePendingTransition(R.anim.right_in, R.anim.right_out);
+		}
+		break;
+		case R.id.my_account_arrow:
+		case R.id.my_account: {
+			Intent intent = new Intent (this, SigninActivity.class);			
+			startActivity(intent);
+			overridePendingTransition(R.anim.right_in, R.anim.right_out);
+		}
+		break;
+		case R.id.add_event_button:
+		case R.id.add_event_text: {
+			Intent intent = new Intent (MainActivity.this, AddEventActivity.class);			
+			startActivity(intent);
+		}
+		break;
+		}
+	}
+	
 	public int getScreenWidth() {
 		return screenWidth;
 	}
@@ -150,7 +184,7 @@ public class MainActivity extends Activity implements View.OnClickListener{
 	/* Class to be defined
 	 * 1. MyOnClickListener (for pager index)
 	 * 2. MyOnPageChangeListener (for tab change)
-	 * 3. OnClick (for buttons)
+	 * 3. MyOnTouchListener (for add event button)
 	 */
 	public class MyOnClickListener implements View.OnClickListener {
 		private int index = 0;
@@ -171,32 +205,41 @@ public class MainActivity extends Activity implements View.OnClickListener{
 			switch (index) {
 			case 0:
 				tab0.setImageDrawable(getResources().getDrawable(R.drawable.tab_list_pressed));
+				text0.setTextColor(Color.parseColor("#3399FF"));
 				if (currentTabIndex == 1) {
 					animation = new TranslateAnimation(animationShiftOneScale, 0, 0, 0);
 					tab1.setImageDrawable(getResources().getDrawable(R.drawable.tab_map_normal));
+					text1.setTextColor(Color.parseColor("#000000"));
 				} else if (currentTabIndex == 2) {
 					animation = new TranslateAnimation(animationShiftTwoScale, 0, 0, 0);
 					tab2.setImageDrawable(getResources().getDrawable(R.drawable.tab_settings_normal));
+					text2.setTextColor(Color.parseColor("#000000"));
 				}
 				break;
 			case 1:
 				tab1.setImageDrawable(getResources().getDrawable(R.drawable.tab_map_pressed));
+				text1.setTextColor(Color.parseColor("#3399FF"));
 				if (currentTabIndex == 0) {
 					animation = new TranslateAnimation(0, animationShiftOneScale, 0, 0);
 					tab0.setImageDrawable(getResources().getDrawable(R.drawable.tab_list_normal));
+					text0.setTextColor(Color.parseColor("#000000"));
 				} else if (currentTabIndex == 2) {
 					animation = new TranslateAnimation(animationShiftTwoScale, animationShiftOneScale, 0, 0);
 					tab2.setImageDrawable(getResources().getDrawable(R.drawable.tab_settings_normal));
+					text2.setTextColor(Color.parseColor("#000000"));
 				}
 				break;
 			case 2:
 				tab2.setImageDrawable(getResources().getDrawable(R.drawable.tab_settings_pressed));
+				text2.setTextColor(Color.parseColor("#3399FF"));
 				if (currentTabIndex == 0) {
 					animation = new TranslateAnimation(0, animationShiftTwoScale, 0, 0);
 					tab0.setImageDrawable(getResources().getDrawable(R.drawable.tab_list_normal));
+					text0.setTextColor(Color.parseColor("#000000"));
 				} else if (currentTabIndex == 1) {
 					animation = new TranslateAnimation(animationShiftOneScale, animationShiftTwoScale, 0, 0);
 					tab1.setImageDrawable(getResources().getDrawable(R.drawable.tab_map_normal));
+					text1.setTextColor(Color.parseColor("#000000"));
 				}
 				break;
 				
@@ -217,34 +260,23 @@ public class MainActivity extends Activity implements View.OnClickListener{
 		}
 	}
 	
+	public class MyOnTouchListener implements View.OnTouchListener {
 
-	@Override
-	public void onClick(View v) {
-		switch(v.getId()) {
-		case R.id.about_us_arrow:
-		case R.id.about_us: {
-			Intent intent = new Intent (this, AboutusActivity.class);			
-			startActivity(intent);
-			overridePendingTransition(R.anim.right_in, R.anim.right_out);
+		@Override
+		public boolean onTouch(View view, MotionEvent event) {
+			switch(event.getAction())
+            {
+            case MotionEvent.ACTION_DOWN :
+            	addView.setImageResource(R.drawable.add_pressed);
+            	addText.setTextColor(Color.parseColor("#3399FF"));
+                break;
+            case MotionEvent.ACTION_UP :
+            	addView.setImageResource(R.drawable.add_normal);
+            	addText.setTextColor(Color.parseColor("#000000"));
+                break;
+            }
+			return false;
 		}
-		break;
-		case R.id.my_account_arrow:
-		case R.id.my_account: {
-			Intent intent = new Intent (this, SigninActivity.class);			
-			startActivity(intent);
-			overridePendingTransition(R.anim.right_in, R.anim.right_out);
-		}
-		break;
-		case R.id.add_event_button1:
-		case R.id.add_event_button2: {
-			Intent intent = new Intent (MainActivity.this, AddEventActivity.class);			
-			startActivity(intent);
-		}
-		break;
-		}
-		
-			
 		
 	}
-	
 }
