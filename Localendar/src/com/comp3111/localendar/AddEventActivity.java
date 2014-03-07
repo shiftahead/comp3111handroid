dfefdd24f5d89051dac0344d99eb4af77dbcf391
jeremy.yangji <jeremy.yangji@gmail.com>
2014-03-07 18:20:57 +0000

    Delete unrelated import packages

--- Localendar/src/com/comp3111/localendar/AddEventActivity.java
+++ Localendar/src/com/comp3111/localendar/AddEventActivity.java
@@ -1,7 +1,6 @@
 package com.comp3111.localendar;
 
 import android.app.Activity;
-import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.view.WindowManager;
---
+++ Localendar/src/com/comp3111/localendar/Calendar.java
@@ -0,0 +1,14 @@
+package com.comp3111.localendar;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class Calendar extends Activity{
+
+	protected void onCreate(Bundle savedInstanceState) {
+		
+		// map activities  
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.tab_calendar);
+	}
+}
