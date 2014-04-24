6d4a4174114a3103df7df5bf359182e5d2d3dba2
zhangtian1000 <zhangtian1000@gmail.com>
2014-04-24 12:56:13 +0000

    new test

--- LocalendarTest/bin/LocalendarTest-debug-unaligned.apk.d
+++
@@ -1,5 +0,0 @@
-/Users/zhangtian/Documents/workspace/LocalendarTest/bin/LocalendarTest-debug-unaligned.apk : \
-/Users/zhangtian/Documents/workspace/LocalendarTest/bin/LocalendarTest.ap_ \
-/Users/zhangtian/Documents/workspace/LocalendarTest/bin/classes.dex \
-/Users/zhangtian/Documents/workspace/LocalendarTest/libs/robotium-solo-5.1.jar \
-/Users/zhangtian/Documents/workspace/LocalendarTest/libs/android-junit-report-1.5.8.jar \
