106af177f0f02637f4aaf31feddd66fbcd0a9719
zhangtian1000 <zhangtian1000@gmail.com>
2014-05-04 10:06:14 +0000

    test of setting activity

--- LocalendarTest/src/com/comp3111/localendar/test/settingActivityTest.java
+++ LocalendarTest/src/com/comp3111/localendar/test/settingActivityTest.java
@@ -1,9 +1,17 @@
 package com.comp3111.localendar.test;
 
+import com.comp3111.localendar.AboutusActivity;
 import com.comp3111.localendar.SettingsActivity;
+import com.comp3111.localendar.SigninActivity;
+import com.comp3111.localendar.calendar.EventDetailActivity;
+import com.comp3111.localendar.facebook.FacebookLogin;
 import com.robotium.solo.Solo;
 
+import android.app.ActionBar;
+import android.app.Activity;
+import android.app.Instrumentation;
 import android.test.ActivityInstrumentationTestCase2;
+import android.test.TouchUtils;
 
 public class settingActivityTest extends ActivityInstrumentationTestCase2<SettingsActivity>{
 	private SettingsActivity mSettingsActivity;
@@ -52,4 +60,37 @@ public class settingActivityTest extends ActivityInstrumentationTestCase2<Settin
     	solo.clickOnText("Vibrate");
     	Thread.sleep(500);
     	}
+    public void testSinginGooglePlus() throws InterruptedException {
+    	Instrumentation instrumentation = getInstrumentation();
+	    Instrumentation.ActivityMonitor monitor = instrumentation.addMonitor(SigninActivity.class.getName(), null, false);
+    	solo.clickOnText("Sign in with Google+");
+    	Thread.sleep(500);
+    	Activity currentActivity = getInstrumentation().waitForMonitorWithTimeout(monitor, 500);
+	    assertNotNull(currentActivity);
+        solo.clickOnActionBarHomeButton();   
+	    currentActivity.finish();
+    	
+	}
+    public void testFacebook() throws InterruptedException {
+    	Instrumentation instrumentation = getInstrumentation();
+	    Instrumentation.ActivityMonitor monitor = instrumentation.addMonitor(FacebookLogin.class.getName(), null, false);
+    	solo.clickOnText("Facebook");
+    	Thread.sleep(500);
+    	Activity currentActivity = getInstrumentation().waitForMonitorWithTimeout(monitor, 500);
+	    assertNotNull(currentActivity);
+        solo.clickOnActionBarHomeButton();  
+	    currentActivity.finish();
+    	
+	}
+    public void testAboutUS() throws InterruptedException {
+    	Instrumentation instrumentation = getInstrumentation();
+	    Instrumentation.ActivityMonitor monitor = instrumentation.addMonitor(AboutusActivity.class.getName(), null, false);
+    	solo.clickOnText("About us");
+    	Thread.sleep(500);
+    	Activity currentActivity = getInstrumentation().waitForMonitorWithTimeout(monitor, 500);
+	    assertNotNull(currentActivity);
+        solo.clickOnActionBarHomeButton(); 
+	    currentActivity.finish();
+    	
+	}
 }
